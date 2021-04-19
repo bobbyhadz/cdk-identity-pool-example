fb2449cf3c84c1533366d9cab17c9269d8080ef7
Borislav Hadzhiev <opmftw@gmail.com>
2021-04-19 19:15:52 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# AWS CDK Cognito User Pool Example - Complete Guide
+
+A repository for an article on
+[bobbyhadz.com](https://bobbyhadz.com/blog/aws-cdk-cognito-user-pool-example)
+
+## How to Use
+
+1. Clone the repository
+
+2. Install the dependencies
+
+```bash
+npm install
+```
+
+3. Create the CDK stack
+
+```bash
+npx cdk deploy
+```
+
+4. Open the AWS Console and the stack should be created in your default region
+
+5. Cleanup
+
+```bash
+npx cdk destroy
+```
---
+++ lib/cdk-starter-stack.ts
@@ -0,0 +1,139 @@
+import * as cognito from '@aws-cdk/aws-cognito';
+import * as iam from '@aws-cdk/aws-iam';
+import * as cdk from '@aws-cdk/core';
+
+export class CdkStarterStack extends cdk.Stack {
+  constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // 👇 User Pool
+    const userPool = new cognito.UserPool(this, 'user-pool', {
+      userPoolName: 'my-user-pool',
+      selfSignUpEnabled: true,
+      signInAliases: {
+        email: true,
+      },
+      autoVerify: {
+        email: true,
+      },
+      passwordPolicy: {
+        minLength: 6,
+        requireLowercase: true,
+        requireDigits: true,
+        requireUppercase: false,
+        requireSymbols: false,
+      },
+      accountRecovery: cognito.AccountRecovery.EMAIL_ONLY,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+
+    // 👇 User Pool Client
+    const userPoolClient = new cognito.UserPoolClient(
+      this,
+      'user-pool-client',
+      {
+        userPool,
+        authFlows: {
+          adminUserPassword: true,
+          custom: true,
+          userSrp: true,
+        },
+        supportedIdentityProviders: [
+          cognito.UserPoolClientIdentityProvider.COGNITO,
+        ],
+      },
+    );
+
+    // 👇 Identity Pool
+    const identityPool = new cognito.CfnIdentityPool(this, 'identity-pool', {
+      identityPoolName: 'my-identity-pool',
+      allowUnauthenticatedIdentities: true,
+      cognitoIdentityProviders: [
+        {
+          clientId: userPoolClient.userPoolClientId,
+          providerName: userPool.userPoolProviderName,
+        },
+      ],
+    });
+
+    const isAnonymousCognitoGroupRole = new iam.Role(
+      this,
+      'anonymous-group-role',
+      {
+        description: 'Default role for anonymous users',
+        assumedBy: new iam.FederatedPrincipal(
+          'cognito-identity.amazonaws.com',
+          {
+            StringEquals: {
+              'cognito-identity.amazonaws.com:aud': identityPool.ref,
+            },
+            'ForAnyValue:StringLike': {
+              'cognito-identity.amazonaws.com:amr': 'authenticated',
+            },
+          },
+          'sts:AssumeRoleWithWebIdentity',
+        ),
+        managedPolicies: [
+          iam.ManagedPolicy.fromAwsManagedPolicyName(
+            'service-role/AWSLambdaBasicExecutionRole',
+          ),
+        ],
+      },
+    );
+
+    const isUserCognitoGroupRole = new iam.Role(this, 'users-group-role', {
+      description: 'Default role for authenticated users',
+      assumedBy: new iam.FederatedPrincipal(
+        'cognito-identity.amazonaws.com',
+        {
+          StringEquals: {
+            'cognito-identity.amazonaws.com:aud': identityPool.ref,
+          },
+          'ForAnyValue:StringLike': {
+            'cognito-identity.amazonaws.com:amr': 'authenticated',
+          },
+        },
+        'sts:AssumeRoleWithWebIdentity',
+      ),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName(
+          'service-role/AWSLambdaBasicExecutionRole',
+        ),
+      ],
+    });
+
+    new cognito.CfnIdentityPoolRoleAttachment(
+      this,
+      'identity-pool-role-attachment',
+      {
+        identityPoolId: identityPool.ref,
+        roles: {
+          authenticated: isUserCognitoGroupRole.roleArn,
+          unauthenticated: isAnonymousCognitoGroupRole.roleArn,
+        },
+        roleMappings: {
+          mapping: {
+            type: 'Token',
+            ambiguousRoleResolution: 'AuthenticatedRole',
+            identityProvider: `cognito-idp.${
+              cdk.Stack.of(this).region
+            }.amazonaws.com/${userPool.userPoolId}:${
+              userPoolClient.userPoolClientId
+            }`,
+          },
+        },
+      },
+    );
+
+    // 👇 Outputs
+    new cdk.CfnOutput(this, 'userPoolId', {
+      value: userPool.userPoolId,
+    });
+    new cdk.CfnOutput(this, 'userPoolClientId', {
+      value: userPoolClient.userPoolClientId,
+    });
+    new cdk.CfnOutput(this, 'identityPoolId', {
+      value: identityPool.ref,
+    });
+  }
+}
